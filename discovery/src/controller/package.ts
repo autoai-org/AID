24fe39dc7cd47d8adaa4eea77acec9dfe6f228da
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-11-06 21:23:28 +0800

    [wip] fixing the bug of reading requests

--- discovery/src/controller/package.ts
+++ discovery/src/controller/package.ts
@@ -3,13 +3,8 @@ import { Package } from '../dynamo/entity';
 import { importPackageRequest } from './entity'
 export default class PackageController {
     public static async importPackage (ctx: Context) {
-        const toImportPackage : Package = new Package();
-        console.log(typeof(ctx.request.body));
-
-        let ipr:importPackageRequest;
-        Object.assign(ipr, ctx.request.body);
-        toImportPackage.isSymbol = true;
-        toImportPackage.linkedTo = ipr.linkedTo;
+        const toImportPackage:Package = new Package();
+        
         console.log(toImportPackage)
     }
 }
