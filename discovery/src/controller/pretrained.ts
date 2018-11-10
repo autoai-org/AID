853a7970388f110d57a27885bbb35753ced12f3e
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-11-10 16:19:39 +0800

    fix several problems

--- discovery/src/controller/pretrained.ts
+++ discovery/src/controller/pretrained.ts
@@ -5,14 +5,14 @@ export default class PackageController {
         putPretrained('yolo_tiny', 'https://premium.file.cvtron.xyz/data/yolo_tiny.ckpt');
         ctx.status = 200;
         ctx.body = {
-            'code': '200',
+            'code': 200,
             'desc': 'success'
         };
     }
     public static async getAllPretrained (ctx: Context) {
         ctx.status = 200;
         ctx.body = {
-            'code': '200',
+            'code': 200,
             'results': await getPretrained()
         };
     }
