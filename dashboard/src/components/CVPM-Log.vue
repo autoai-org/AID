756cb7a709ad952058970decd621dff0c50bcb61
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-12-04 22:00:10 +0800

    finish dashboard displaying system log

--- dashboard/src/components/CVPM-Log.vue
+++ dashboard/src/components/CVPM-Log.vue
@@ -1,18 +1,40 @@
 <template>
-    <v-card>
-        <div class="terminal-window">
-
-        </div>
-    </v-card>
+  <div class="terminal">
+    <div class="header">
+      <h4>{{title}}</h4>
+    </div>
+    <div
+      style="position:absolute;top:0;left:0;right:0;overflow:auto;z-index:1;margin-top:30px;max-height:500px"
+      ref="terminalWindow"
+    >
+      <div class="terminal-window" id="terminalWindow">
+        <p v-for="(item, index) in messageList" :key="index">
+          <span class="cmd">{{item}}</span>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+    }
+  },
+  props: ['title', 'messageList']
 }
 </script>
 
 <style lang="scss" scoped>
+.terminal {
+  position: relative;
+  width: 100%;
+  border-radius: 4px;
+  color: white;
+  margin-bottom: 10px;
+  max-height: 580px;
+}
 .terminal .terminal-window {
   padding-top: 50px;
   background-color: rgb(3, 9, 36);
@@ -56,5 +78,125 @@ export default {
   pre {
     display: inline;
   }
+}
+.terminal .header ul.shell-dots li {
+  display: inline-block;
+  width: 12px;
+  height: 12px;
+  border-radius: 6px;
+  background-color: rgb(3, 9, 36);
+  margin-left: 6px;
+}
+.terminal .header ul.shell-dots li.red {
+  background-color: rgb(200, 48, 48);
+}
+.terminal .header ul.shell-dots li.yellow {
+  background-color: rgb(247, 219, 96);
+}
+.terminal .header ul.shell-dots li.green {
+  background-color: rgb(46, 201, 113);
+}
+.terminal .header {
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  right: 0;
+  left: 0;
+  background-color: rgb(149, 149, 152);
+  text-align: center;
+  padding: 2px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+}
+.terminal .header h4 {
+  font-size: 14px;
+  margin: 5px;
+  letter-spacing: 1px;
+}
+.terminal .header ul.shell-dots {
+  position: absolute;
+  top: 5px;
+  left: 8px;
+  padding-left: 0;
+  margin: 0;
+}
+.terminal .terminal-window .prompt::before {
+  content: "$";
+  margin-right: 10px;
+}
+.terminal .terminal-window .cursor {
+  margin: 0;
+  background-color: white;
+  animation: blink 1s step-end infinite;
+  -webkit-animation: blink 1s step-end infinite;
+  margin-left: -5px;
+}
+@keyframes blink {
+  50% {
+    visibility: hidden;
+  }
+}
+@-webkit-keyframes blink {
+  50% {
+    visibility: hidden;
+  }
+}
+.terminal .terminal-window .loading {
+  display: inline-block;
+  width: 0;
+  overflow: hidden;
+  animation: load 1.2s step-end infinite;
+  -webkit-animation: load 1.2s step-end infinite;
+}
+@keyframes load {
+  0% {
+    width: 0px;
+  }
+  20% {
+    width: 5px;
+  }
+  40% {
+    width: 10px;
+  }
+  60% {
+    width: 15px;
+  }
+  80% {
+    width: 20px;
+  }
+}
+@-webkit-keyframes load {
+  0% {
+    width: 0px;
+  }
+  20% {
+    width: 5px;
+  }
+  40% {
+    width: 10px;
+  }
+  60% {
+    width: 15px;
+  }
+  80% {
+    width: 20px;
+  }
+}
+.terminal-last-line {
+  font-size: 0;
+  word-spacing: 0;
+  letter-spacing: 0;
+}
+.input-box {
+  position: relative;
+  background: rgb(3, 9, 36);
+  border: none;
+  width: 1px;
+  opacity: 0;
+  cursor: default;
+}
+.input-box:focus {
+  outline: none;
+  border: none;
 }
 </style>
--- dashboard/src/services/web.js
+++ dashboard/src/services/web.js
@@ -1,10 +1,11 @@
 function getMenus () {
   return [
         { 'header': 'Admin' },
-        { 'href': '/', 'title': 'Home', 'icon': 'home' },
+        { 'href': '/home', 'title': 'Home', 'icon': 'home' },
         { 'href': '/local/package', 'title': 'Packages', 'icon': 'view_list' },
         { 'href': '/local/pretrained', 'title': 'Pre-trained', 'icon': 'view_list' },
         { 'header': 'System' },
+        { 'href': '/log', 'title': 'Log', 'icon': 'info' },
         { 'href': '/settings', 'title': 'Settings', 'icon': 'settings' },
         { 'href': '/login', 'icon': 'lock', 'title': 'Logout' }
   ]
