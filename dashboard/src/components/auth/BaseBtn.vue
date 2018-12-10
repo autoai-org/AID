a9be06055976b1a85e26b4e4d6304abdfb988a4a
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-12-10 20:21:16 +0800

    add authentication

---
+++ dashboard/src/components/auth/BaseBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <VBtn
+    class="white--text"
+    color="indigo"
+    depressed
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </VBtn>
+</template>
+
+<script>
+  export default {
+    inheritAttrs: false
+  }
+</script>
+
+<style lang="scss" scoped>
+  .v-btn {
+    border-radius: 4px;
+    text-transform: none;
+  }
+</style>
