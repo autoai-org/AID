e8a5b11bb55ac66bebfa460d449f5a1e0ba8e546
Reece Dunham <me@rdil.rocks>
2019-01-23 12:07:44 -0500

    Setup.py: added license metadata

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/unarxiv/cvpm",
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
