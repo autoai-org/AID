91e34f0d37c74901945f7764a083894b574b9cf0
Xiaozhe <407718364@qq.com>
2019-04-06 17:59:16 +0800

    try fix travis

--- cli/build.sh
+++ cli/build.sh
@@ -6,6 +6,8 @@ if [[ -z "$package" ]]; then
 fi
 package_name="cvpm"
 platforms=("windows/amd64" "darwin/amd64" "linux/amd64" "linux/arm64" "linux/mips" "freebsd/amd64")
+VERSION=`git describe --tags`
+BUILD=`date +%FT%T%z`
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
@@ -16,7 +18,7 @@ do
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
-    env GOOS=$GOOS GOARCH=$GOARCH go build -ldflags "-s -w" -o 'dist/'$output_name $package
+    env GOOS=$GOOS GOARCH=$GOARCH go build -ldflags "-s -w -X main.Version=${VERSION} -X main.Build=${BUILD}"  -o 'dist/'$output_name $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
