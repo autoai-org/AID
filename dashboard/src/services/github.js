1b9aef561345f9b1755230aebf9ad3ec408cf479
cyy <sheeppooo.cy@gmail.com>
2019-06-24 09:47:02 +0800

    replace github queries to _get and _post

--- dashboard/src/services/github.js
+++ dashboard/src/services/github.js
@@ -1,34 +1,17 @@
-import axios from 'axios'
+import { _get, _post } from './base'
+
 class GithubService {
   constructor (repo) {
     this.repo = repo
   }
   fetchCVPMConfig () {
-    return new Promise((resolve, reject) => {
-      axios.get('https://api.github.com/repos/' + this.repo + '/contents/cvpm.toml').then(function (res) {
-        resolve(res)
-      }).catch(function (err) {
-        reject(err)
-      })
-    })
+    return _get('https://api.github.com/repos/' + this.repo + '/contents/cvpm.toml')
   }
   fetchDependency () {
-    return new Promise((resolve, reject) => {
-      axios.get('https://api.github.com/repos/' + this.repo + '/contents/requirements.txt').then(function (res) {
-        resolve(res)
-      }).catch(function (err) {
-        reject(err)
-      })
-    })
+    return _get('https://api.github.com/repos/' + this.repo + '/contents/requirements.txt')
   }
   fetchReadme () {
-    return new Promise((resolve, reject) => {
-      axios.get('https://api.github.com/repos/' + this.repo + '/readme').then(function (res) {
-        resolve(res)
-      }).catch(function (err) {
-        reject(err)
-      })
-    })
+    return _get('https://api.github.com/repos/' + this.repo + '/readme')
   }
 }
 
