24ff6c4e3dfb3c55910298329b7fab055e564597
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2019-12-14 21:26:28 +0100

    [wip] framework

--- components/dashboard/src/services/store.ts
+++ components/dashboard/src/services/store.ts
@@ -1,5 +1,23 @@
 import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger'
 import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
+import reducer from './reducer'
 
 export const history = createHistory();
+
+const myRouterMiddleware = routerMiddleware(history);
+
+const getMiddleware = () => {
+    if (process.env.NODE_ENV === 'production') {
+        return applyMiddleware(myRouterMiddleware);
+    } else {
+        // Enable additional logging in non-production environments.
+        return applyMiddleware(myRouterMiddleware, createLogger())
+    }
+};
+
+export const store = createStore(
+    reducer, composeWithDevTools(getMiddleware()));
