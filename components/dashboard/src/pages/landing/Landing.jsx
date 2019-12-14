1d9c76d0764e338ad681ba5b1508999aff7bfab4
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2019-12-14 21:14:50 +0100

    add ci for dashboard

---
+++ components/dashboard/src/pages/landing/Landing.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Landing extends React.Component {
+
+}
+
+export default Landing
---
+++ components/dashboard/src/services/store.ts
@@ -0,0 +1,5 @@
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger'
+import createHistory from 'history/createBrowserHistory';
+
+export const history = createHistory();
