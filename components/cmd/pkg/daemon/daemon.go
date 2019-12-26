e0d8bbad0d280608bcb548e77f4e10fe927d331c
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2019-12-26 01:57:51 +0100

    add daemon

--- components/cmd/pkg/daemon/daemon.go
+++ components/cmd/pkg/daemon/daemon.go
@@ -1,1 +1,81 @@
 package daemon
+
+import (
+	"log"
+	"os"
+	"os/user"
+	"runtime"
+
+	"github.com/kardianos/service"
+)
+
+// DaemonPort is the default port that the daemon will be listening at
+const DaemonPort = "10590"
+
+type sol struct {
+}
+
+func (s *sol) Start(srv service.Service) error {
+	go RunServer(DaemonPort)
+	return nil
+}
+
+func (s *sol) Stop(srv service.Service) error {
+	return nil
+}
+
+func getRunUser() string {
+	currentUser, _ := user.Current()
+	if currentUser.Username == "root" && runtime.GOOS != "windows" {
+		return os.Getenv("SUDO_USER")
+	}
+	return currentUser.Username
+}
+
+func getAIPMDConfig() *service.Config {
+	realUsername := getRunUser()
+	srvConf := &service.Config{
+		Name:        "aipmd",
+		DisplayName: "AIPM Daemon",
+		Description: "Artificial Intelligence Package Manager[Daemon]",
+		Arguments:   []string{"daemon", "up"},
+		UserName:    realUsername,
+	}
+	return srvConf
+}
+
+// Install adds the server into the background service
+func Install() {
+	srvConfig := getAIPMDConfig()
+	dae := &sol{}
+	s, err := service.New(dae, srvConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Install()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Uninstall removes the background daemon service
+func Uninstall() {
+	srvConfig := getAIPMDConfig()
+	dae := &sol{}
+	s, err := service.New(dae, srvConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Stop()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Uninstall()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
