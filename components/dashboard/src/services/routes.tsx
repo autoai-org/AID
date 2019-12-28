a1a896849aab1cbb24e981ce2d83ec09290981c4
cyy <sheeppooo.cy@gmail.com>
2019-12-28 22:34:31 +0100

    fix style problem

--- components/dashboard/src/services/routes.tsx
+++ components/dashboard/src/services/routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
 import Home from '../pages/home/Home'
+import Dashboard from '../pages/dashboard/Dashboard'
 
 const routes = (
   <div>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   </div>
 )
