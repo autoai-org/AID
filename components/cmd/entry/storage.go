c66192cd84c3dab11f4a6560b1fcaaca2e8778a3
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2019-12-28 02:38:53 +0100

    fix licensing

---
+++ components/cmd/entry/storage.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2019 Xiaozhe Yao & AICAMP.CO.,LTD
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package main
+
+import (
+	"github.com/autoai-org/aiflow/components/cmd/pkg/storage"
+	"github.com/autoai-org/aiflow/components/cmd/pkg/entities"
+	"github.com/ilibs/gosql/v2"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func save() {
+	db := storage.NewDB("sqlite3", "./db.db")
+	db.Connect()
+	packages := &entities.Package{}
+	gosql.Model(packages).Where("id=?",1).Get()
+}
