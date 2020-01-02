c6f28522a664f4f46ebcc375ee8c9a07badae249
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-01-03 00:57:53 +0100

    relicense

--- components/dashboard/tests/unit/app.spec.ts
+++ components/dashboard/tests/unit/app.spec.ts
@@ -2,14 +2,12 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
-import vuetify from '@/plugins/vuetify';
 
 describe('App.vue', () => {
   it('renders title when created', () => {
     const wrapper = shallowMount(App, {
       router,
       store,
-      vuetify,
     })
     expect(wrapper.find('.title').text()).toMatch("AI Flow")
   })
