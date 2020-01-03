961ba573461bca059eb90a980843ab370bc9d036
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-01-03 03:00:42 +0100

    fix .sh file format

--- components/cmd/build.sh
+++ components/cmd/build.sh
@@ -1,22 +1,23 @@
+#!/bin/bash
 package_name="aiflow"
 platforms=("windows/amd64" "darwin/amd64" "linux/amd64" "linux/arm64")
-VERSION=`git describe --tags`
-BUILD=`date +%FT%T%z`
+VERSION="$(git describe --tags)"
+BUILD="$(date +%FT%T%z)"
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    echo 'Building AIFlow Command Line Utility for '$GOOS'/'$GOARCH'...'
+    echo "Building AIFlow Command Line Utility for '$GOOS'/'$GOARCH'..."
     output_name=$package_name'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
-        output_name+='.exe'
+        output_name+=".exe"
     fi
     env GOOS=$GOOS GOARCH=$GOARCH go build -ldflags "-s -w -X main.Version=${VERSION} -X main.Build=${BUILD}"  -o 'dist/'$output_name entry/*.go
     if [ $? -ne 0 ]; then
-        echo 'An error has occurred! Aborting the script execution...'
+        echo "An error has occurred! Aborting the script execution..."
         exit 1
     fi
 done
 
-echo 'Successfully built AIFlow Command Line Utility, please go to components/cmd/dist/'
+echo "Successfully built AIFlow Command Line Utility, please go to components/cmd/dist/"
