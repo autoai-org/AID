55eaab6a5a9184451b59d272d0a7f95f4073e9e8
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-01-23 15:44:48 +0100

    add AID_PROD env to set release mode

--- components/cmd/pkg/daemon/router.go
+++ components/cmd/pkg/daemon/router.go
@@ -6,10 +6,14 @@
 package daemon
 
 import (
+	"os"
 	"github.com/gin-gonic/gin"
 )
 
 func getRouter() *gin.Engine {
+	if (os.Getenv("AID_PROD")=="true") {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	r := gin.Default()
 	r.Use(beforeResponse())
 	r.Use(gin.Recovery())
