3588ec03feac9cfc675908ca05ebe12e635472cb
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-03-09 23:54:59 +0100

    update mongoose settings

--- components/discovery/src/models/users.model.ts
+++ components/discovery/src/models/users.model.ts
@@ -3,21 +3,22 @@
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 import { Application } from '../declarations';
-
+import mongoose from 'mongoose'
 export default function (app: Application) {
   const modelName = 'users';
   const mongooseClient = app.get('mongooseClient');
   const schema = new mongooseClient.Schema({
-  
-  
+    username: { type: String, index: true, unique: true, required: [true, 'can\'t be blank'], match: [/^[a-zA-Z0-9_-]+$/, 'is invalid'] },
+    email: { type: String, index: true, unique: true, required: [true, 'can\'t be blank'], match: [/\S+@\S+\.\S+/, 'is invalid'] },
+    password: String,
+    bio: String,
+    image: String,
+    followingList: [mongoose.Schema.Types.ObjectId],
     googleId: { type: String },
-  
     facebookId: { type: String },
-  
     twitterId: { type: String },
-  
     githubId: { type: String },
-  
+
   }, {
     timestamps: true
   });
--- components/discovery/src/mongoose.ts
+++ components/discovery/src/mongoose.ts
@@ -3,12 +3,12 @@ import { Application } from './declarations';
 import logger from './logger';
 
 export default function (app: Application) {
+  console.log(app.get('mongodb'))
   mongoose.connect(
     app.get('mongodb'),
     { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
   ).catch(err => {
     logger.error(err);
-    process.exit(1);
   });
   
   mongoose.Promise = global.Promise;
