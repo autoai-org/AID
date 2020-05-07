a9e9e678e7a7f2b9e9ab7fe78b6047439bbe1ad7
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-05-07 12:20:56 +0200

    add port config

--- components/discovery/src/main.ts
+++ components/discovery/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
+import config from './config'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
@@ -13,6 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(config.PORT);
 }
 bootstrap();
