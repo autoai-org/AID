bcfee8e75f09ebfa5701dd98449028fd34b3cfef
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-10-11 14:15:01 +0200

    add default value for GITHUB_TOKEN

--- components/discovery/src/config.ts
+++ components/discovery/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
     MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/aid",
     PORT: process.env.PORT || 3000,
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN ||"",
     JWT_SECRET: process.env.JWT_SECRET || "autoai-aid",
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
     GOOGLE_SECRET:process.env.GOOGLE_SECRET || "",
