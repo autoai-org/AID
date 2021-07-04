dd2acd8d91c602c1e7df15d4a102365778b68729
Xiaozhe Yao <askxzyao@gmail.com>
2021-07-04 13:32:33 +0200

    [wip] upgrading tauri

--- components/studio/src-tauri/src/main.rs
+++ components/studio/src-tauri/src/main.rs
@@ -8,11 +8,12 @@ mod cmd;
 fn main() {
   tauri::AppBuilder::new()
     .invoke_handler(|_webview, arg| {
+      std::thread::spawn(move || {
+        spawn_aid_server();
+      });
       use cmd::Cmd::*;
       match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
+        Err(e) => Err(e.to_string()),
         Ok(command) => {
           match command {
             // definitions for your custom commands from Cmd here
@@ -27,4 +28,24 @@ fn main() {
     })
     .build()
     .run();
+}
+fn spawn_aid_server() {
+  let (mut rx, mut child) = Command::new_sidecar("my-sidecar")
+    .expect("failed to create `my-sidecar` binary command")
+    .spawn()
+    .expect("Failed to spawn sidecar");
+
+  tauri::async_runtime::spawn(async move {
+    // read events such as stdout
+    while let Some(event) = rx.recv().await {
+      if let CommandEvent::Stdout(line) = event {
+        window
+          .emit("message", Some(format!("'{}'", line)))
+          .expect("failed to emit event");
+        // write to stdin
+        child.write("message from Rust\n".as_bytes()).unwrap();
+      }
+    }
+  });
+  Copy
 }
