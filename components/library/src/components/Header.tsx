5bfb65ac980c32d7d838fb1ee12e4c210da77388
Xiaozhe Yao <askxzyao@gmail.com>
2021-07-12 18:06:23 +0200

    update login and registration

--- components/library/src/components/Header.tsx
+++ components/library/src/components/Header.tsx
@@ -4,6 +4,10 @@ import {
     MenuIcon,
     XIcon,
 } from '@heroicons/react/outline'
+import { FirebaseAuthConsumer } from '@react-firebase/auth'
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useHistory } from "react-router-dom";
 
 const navigation = [
     { name: 'Documents', href: 'https://aid.autoai.org' },
@@ -13,123 +17,159 @@ const navigation = [
 ]
 
 export default function AIDFooter() {
-  return (<Popover as="header" className="relative">
-  {({ open }) => (
-      <>
-          <div className="bg-gray-900 pt-4 pb-4">
-              <nav
-                  className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
-                  aria-label="Global"
-              >
-                  <div className="flex items-center flex-1">
-                      <div className="flex items-center justify-between w-full md:w-auto">
-                          <a href="/">
-                              <span className="sr-only">Workflow</span>
-                              <img
-                                  className="h-8 w-auto sm:h-10"
-                                  src="../logo.png"
-                                  alt=""
-                              />
-                          </a>
-                          <div className="-mr-2 flex items-center md:hidden">
-                              <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
-                                  <span className="sr-only">Open main menu</span>
-                                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                              </Popover.Button>
-                          </div>
-                      </div>
-                      <div className="hidden space-x-8 md:flex md:ml-10">
-                          {navigation.map((item) => (
-                              <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="text-base font-medium text-white hover:text-gray-300"
-                              >
-                                  {item.name}
-                              </a>
-                          ))}
-                      </div>
-                  </div>
-                  <div className="hidden md:flex md:items-center md:space-x-6">
-                      <a href="/" className="text-base font-medium text-white hover:text-gray-300">
-                          Log in
-                      </a>
-                      <a
-                          href="/"
-                          className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
-                      >
-                          Sign up
-                      </a>
-                  </div>
-              </nav>
-          </div>
+    const history = useHistory();
+    return (<Popover as="header" className="relative">
+        {({ open }) => (
+            <>
+                <div className="bg-gray-900 pt-4 pb-4">
+                    <nav
+                        className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+                        aria-label="Global"
+                    >
+                        <div className="flex items-center flex-1">
+                            <div className="flex items-center justify-between w-full md:w-auto">
+                                <a href="/">
+                                    <span className="sr-only">Workflow</span>
+                                    <img
+                                        className="h-8 w-auto sm:h-10"
+                                        src="../logo.png"
+                                        alt=""
+                                    />
+                                </a>
+                                <div className="-mr-2 flex items-center md:hidden">
+                                    <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                                        <span className="sr-only">Open main menu</span>
+                                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                            <div className="hidden space-x-8 md:flex md:ml-10">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className="text-base font-medium text-white hover:text-gray-300"
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                        <FirebaseAuthConsumer>
+                            {({ isSignedIn, user, providerId }) => {
+                                if (isSignedIn === true) {
+                                    return (
+                                        <div className="hidden md:flex md:items-center md:space-x-6">
+                                            <a href="#" className="flex-shrink-0 group block">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <img
+                                                            className="inline-block h-9 w-9 rounded-full"
+                                                            src={user.photoURL}
+                                                            alt=""
+                                                        />
+                                                    </div>
+                                                    <div className="ml-3">
+                                                        <p className="text-sm font-medium text-white group-hover:text-gray-900">{user.displayName}</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a
+                                                onClick={()=>firebase.auth().signOut().then(function(res){
+                                                    history.replace("/");
+                                                })}
+                                                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
+                                            >
+                                                Sign out
+                                            </a>
+                                        </div>);
+                                } else {
+                                    return (
+                                        <div className="hidden md:flex md:items-center md:space-x-6">
+                                            <a href="/login" className="text-base font-medium text-white hover:text-gray-300">
+                                                Log in
+                                            </a>
+                                            <a
+                                                href="/signup"
+                                                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
+                                            >
+                                                Sign up
+                                            </a>
+                                        </div>
+                                    )
+                                }
+                            }}
+                        </FirebaseAuthConsumer>
 
-          <Transition
-              show={open}
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-          >
-              <Popover.Panel
-                  focus
-                  static
-                  className="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
-              >
-                  <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                      <div className="px-5 pt-4 flex items-center justify-between">
-                          <div>
-                              <img
-                                  className="h-8 w-auto"
-                                  src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
-                                  alt=""
-                              />
-                          </div>
-                          <div className="-mr-2">
-                              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
-                                  <span className="sr-only">Close menu</span>
-                                  <XIcon className="h-6 w-6" aria-hidden="true" />
-                              </Popover.Button>
-                          </div>
-                      </div>
-                      <div className="pt-5 pb-6">
-                          <div className="px-2 space-y-1">
-                              {navigation.map((item) => (
-                                  <a
-                                      key={item.name}
-                                      href={item.href}
-                                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-                                  >
-                                      {item.name}
-                                  </a>
-                              ))}
-                          </div>
-                          <div className="mt-6 px-5">
-                              <a
-                                  href="/"
-                                  className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
-                              >
-                                  Search for Models
-                              </a>
-                          </div>
-                          <div className="mt-6 px-5">
-                              <p className="text-center text-base font-medium text-gray-500">
-                                  Existing customer?{' '}
-                                  <a href="/" className="text-gray-900 hover:underline">
-                                      Login
-                                  </a>
-                              </p>
-                          </div>
-                      </div>
-                  </div>
-              </Popover.Panel>
-          </Transition>
-      </>
-  )}
-</Popover>
-    
-  )
+                    </nav>
+                </div>
+
+                <Transition
+                    show={open}
+                    as={Fragment}
+                    enter="duration-150 ease-out"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="duration-100 ease-in"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    <Popover.Panel
+                        focus
+                        static
+                        className="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
+                    >
+                        <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                            <div className="px-5 pt-4 flex items-center justify-between">
+                                <div>
+                                    <img
+                                        className="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="-mr-2">
+                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                                        <span className="sr-only">Close menu</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                            <div className="pt-5 pb-6">
+                                <div className="px-2 space-y-1">
+                                    {navigation.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                                        >
+                                            {item.name}
+                                        </a>
+                                    ))}
+                                </div>
+                                <div className="mt-6 px-5">
+                                    <a
+                                        href="/"
+                                        className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
+                                    >
+                                        Search for Models
+                                    </a>
+                                </div>
+                                <div className="mt-6 px-5">
+                                    <p className="text-center text-base font-medium text-gray-500">
+                                        Existing customer?{' '}
+                                        <a href="/login" className="text-gray-900 hover:underline">
+                                            Login
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Popover.Panel>
+                </Transition>
+            </>
+        )}
+    </Popover>
+
+    )
 }
